9068750644eecb3113d07a9590ba1dc45eddd683
yuliya.kozlovaa <yuliya.kozlovaa@simbirsoft.com>
2024-06-05 21:23:08 +0300

    feat: refactor metrics

--- metrics-producer/src/main/java/com/example/service/KafkaServiceImpl.java
+++ metrics-producer/src/main/java/com/example/service/KafkaServiceImpl.java
@@ -1,7 +1,6 @@
 package com.example.service;
 
 import com.example.feign.ActuatorClient;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import lombok.RequiredArgsConstructor;
@@ -18,33 +17,31 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 public class KafkaServiceImpl implements KafkaService {
 
-    private final ActuatorClient actuatorClient;
-    private final KafkaTemplate<Object, Object> template;
+  private final ActuatorClient actuatorClient;
+  private final KafkaTemplate<Object, Object> template;
 
-    @Value("${message.topic.name}")
-    private String topicName;
+  @Value("${message.topic.name}")
+  private String topicName;
 
-    @Override
-    public void sendMetrics() {
-        List<String> metrics = new ArrayList<>();
-        metrics.add(actuatorClient.getMetric("application.ready.time"));
-        metrics.add(actuatorClient.getMetric("jdbc.connections.active"));
-        metrics.add(actuatorClient.getMetric("jvm.threads.live"));
-        metrics.add(actuatorClient.getMetric("jvm.memory.used"));
-        metrics.add(actuatorClient.getMetric("process.uptime"));
+  @Value("${metrics.list}")
+  private List<String> nameMetrics;
 
-        metrics.forEach(metric -> {
-                    CompletableFuture<SendResult<Object, Object>> future = template.send(topicName, metric);
-                    future.whenComplete((result, ex) -> {
-                        if (ex == null) {
-                            log.info("Sent message=[" + metric + "] with offset=[" +
-                                    result.getRecordMetadata().offset() + "]");
-                        } else {
-                            log.info("Unable to send message=[" + metric + "] due to : " + ex.getMessage());
-                        }
-                    });
-                }
-        );
-    }
+  @Override
+  public void sendMetrics() {
+    List<String> metrics = nameMetrics.stream()
+        .map(actuatorClient::getMetric).toList();
+
+    metrics.forEach(metric -> {
+          CompletableFuture<SendResult<Object, Object>> future = template.send(topicName, metric);
+          future.whenComplete((result, ex) -> {
+            if (ex == null) {
+              log.info("Sent message=[{}] with offset=[{}]", metric, result.getRecordMetadata().offset());
+            } else {
+              log.info("Unable to send message=[{}] due to : {}", metric, ex.getMessage());
+            }
+          });
+        }
+    );
+  }
 
 }
